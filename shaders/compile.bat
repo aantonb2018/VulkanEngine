40be0b770287d33b87308a2388d4458873771f66
aantonb2018 <alexantber@gmail.com>
2025-03-25 20:36:40 +0100

    Initial commit

---
+++ shaders/compile.bat
@@ -0,0 +1,4 @@
+D:\VulkanSDK\1.4.309.0\Bin\glslc.exe vert.vert -o vert.spv
+D:\VulkanSDK\1.4.309.0\Bin\glslc.exe diffuse.frag -o diffuse.spv
+D:\VulkanSDK\1.4.309.0\Bin\glslc.exe composition_v.vert -o composition_v.spv
+D:\VulkanSDK\1.4.309.0\Bin\glslc.exe composition_f.frag -o composition_f.spv
