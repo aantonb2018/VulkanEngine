762992eb005f0f532e67856ec7c03b18392292eb
aantonb2018 <alexantber@gmail.com>
2025-04-14 20:14:28 +0200

    ✨ CookTorrance BRDF

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -2,3 +2,5 @@ D:\VulkanSDK\1.4.309.0\Bin\glslc.exe vert.vert -o vert.spv
 D:\VulkanSDK\1.4.309.0\Bin\glslc.exe diffuse.frag -o diffuse.spv
 D:\VulkanSDK\1.4.309.0\Bin\glslc.exe composition_v.vert -o composition_v.spv
 D:\VulkanSDK\1.4.309.0\Bin\glslc.exe composition_f.frag -o composition_f.spv
+D:\VulkanSDK\1.4.309.0\Bin\glslc.exe microfacets.frag -o microfacets.spv
+pause
